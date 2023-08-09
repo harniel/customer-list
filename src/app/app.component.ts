c07545d9bdad1342fd62e645dfdf2a771eefe819
Harniel Salmeron <harniel@reelmen.com>
2023-08-09 21:32:25 +0800

    initial implementation for customer-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'customer-list';
+  title = 'Customer List';
+  customers: any[] = [];
+
+  reset() {}
+
+  addCustomer(data: any) {
+    const customer = {
+      name: data.value.input,
+    };
+
+    if (customer.name !== '' && customer.name !== undefined) {
+      this.customers.push(customer.name);
+      data.value.input = '';
+    }
+  }
 }
